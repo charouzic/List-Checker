a94ad26b3eee1707c6154781ec793731e5b85eae
Viktor <viki.macek@gmail.com>
2021-01-11 18:40:48 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace list_checker
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
