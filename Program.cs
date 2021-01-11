b1b0f95f60e87cddf139c2007fcbb0bf06927d04
charouzic <viki.macek@gmail.com>
2021-01-11 18:44:06 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
 namespace list_checker
 {
@@ -6,7 +7,32 @@ namespace list_checker
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+
+            List<int> numbers = new List<int>() {-6,-5,10,15};
+
+            Processor p = new Processor();
+
+            Console.WriteLine($"{p.listChecker(numbers)}");
+
+        }
+    }
+    class Processor
+    {
+        public bool listChecker(List<int> numbers)
+        {
+            if (numbers.Count < 2)
+                return true;
+            for (int counter = 1; counter < numbers.Count; counter++)
+            {
+                if (numbers[counter - 1] > numbers[counter])
+                {
+                    return false;
+                }
+                else
+                    continue;
+            }
+            return true;
         }
     }
 }
